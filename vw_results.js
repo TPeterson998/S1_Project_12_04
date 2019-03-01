7a3d5d8a97fd4482e3bb0926075529200d33865d
Trent Peterson <tpeterson998@west-mec.org>
2019-03-01 15:26:35 -0700

    day 1
    my brain is pretty dead right now so I was not able to do much or debug much.

--- vw_results.js
+++ vw_results.js
@@ -5,8 +5,8 @@
    Tutorial 10
    Case Problem 4
 
-   Author: 
-   Date:   
+   Author: Trent Peterson
+   Date:   3/1/19
    
    Filename: vw_results.js
    
@@ -23,18 +23,23 @@
    
       
 */
-
-
-
+var reportHTML = "<h1>" + raceTitle + "</h1>";
+for (var i = 0; i < race.length; i++) {
+    var totalVotes = 0;
+    votes.forEach(calcSum(i))
+    reportHTML += "<table> <caption>" + race[i] + "</caption> <tr><th>Candidate</th><th>Votes</th></tr>";
+    candidateRows([i], totalVotes);
+    reportHTML += "</table>";
+}
+document.getElementsByTagName("section")[0].innerHTML = reportHTML;
 
 
 /* Callback Function to calculate an array sum */
 function calcSum(value) {
-   totalVotes += value;
+    totalVotes += value;
 }
 
 /* Function to calculate a percentage */
 function calcPercent(value, sum) {
-   return (100*value/sum);
+    return (100 * value / sum);
 }
-
